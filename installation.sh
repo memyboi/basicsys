5bfca5e7b6c06f96cc8031f8da21eebb9bad677a
deadfry42 <75206733+memyboi@users.noreply.github.com>
2024-02-24 14:45:02 +0000

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,5 +1,5 @@
 sudo chmod u+x ./basicsys/basicsys
 sudo mv ./basicsys/basicsys /usr/bin
 sudo rm -rf ./basicsys
-echo basicsys is now installed!
+echo -e ${'\033[0;32m'}basicsys is now installed!
 basicsys
