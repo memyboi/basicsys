278e8f73cbb8dba9d96021a488470cd5956ab034
deadfry42 <75206733+memyboi@users.noreply.github.com>
2024-02-24 14:46:37 +0000

    hopefully final commit

--- installation.sh
+++ installation.sh
@@ -2,6 +2,6 @@ GREEN='\033[0;32m'
 sudo chmod u+x ./basicsys/basicsys
 sudo mv ./basicsys/basicsys /usr/bin
 sudo rm -rf ./basicsys
-echo -e ${GREEN}basicsys is now installed!
-echo -e ${GREEN}run the command 'basicsys' to see basic system information!
+echo -e "${GREEN}basicsys is now installed!"
+echo -e "${GREEN}run the command 'basicsys' to see basic system information!"
 basicsys
