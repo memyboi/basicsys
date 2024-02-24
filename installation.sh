29fbb8d6a4724716004c1ee036792fd6de24a9f2
deadfry42 <75206733+memyboi@users.noreply.github.com>
2024-02-24 14:38:59 +0000

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,7 +1,2 @@
-mkdir basicsysinstallation
-cd basicsysinstallation
-git clone https://github.com/memyboi/basicsys.git --depth 1
-rm basicsys/README.md
-sudo cp basicsys/basicsys /usr/bin
-cd ..
-rm -rf basicsysinstallation
+sudo mv basicsys/basicsys /usr/bin
+rm -rf basicsys
